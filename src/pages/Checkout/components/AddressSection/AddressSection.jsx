e5dc7bfd4e313cb91ab101bd46c2e6d7fe4bafd6
Nahuel <zeballosnahuelfd@gmail.com>
2025-03-21 18:01:15 -0300

    fix 3/3 checkout add address modal

--- src/pages/Checkout/components/AddressSection/AddressSection.jsx
+++ src/pages/Checkout/components/AddressSection/AddressSection.jsx
@@ -1,54 +1,67 @@
-import "./AddressSection.css";
-import React from "react";
+import './AddressSection.css';
+import React from 'react';
 
-import { useAddress } from "../../../../contexts/AddressProvider.js";
-import { useUserData } from "../../../../contexts/UserDataProvider.js";
-import { AddressModal } from "../AddressModal/AddressModal";
+import { useAddress } from '../../../../contexts/AddressProvider.js';
+import { useUserData } from '../../../../contexts/UserDataProvider.js';
+import { AddressModal } from '../AddressModal/AddressModal';
 
 export const AddressSection = () => {
-  const { userDataState, dispatch } = useUserData();
+	const { userDataState, dispatch } = useUserData();
 
-  const { isAddressModalOpen, setIsAddressModalOpen } = useAddress();
+	const { isAddressModalOpen, setIsAddressModalOpen } = useAddress();
 
-  return (
-    <div className="address-container">
-      {userDataState.addressList?.map((address, index) => {
-        const { name, street, city, state, country, pincode, phone, _id } =
-          address;
+	return (
+		<div className='address-container'>
+			{userDataState.addressList?.map((address, index) => {
+				const {
+					name,
+					street,
+					city,
+					state,
+					country,
+					pincode,
+					phone,
+					_id,
+				} = address;
 
-        return (
-          <div key={_id} className="address-card">
-            <input
-              checked={_id === userDataState.orderDetails?.orderAddress?._id}
-              onChange={() => {
-                dispatch({
-                  type: "SET_ORDER",
-                  payload: { orderAddress: address },
-                });
-              }}
-              name="address"
-              id={_id}
-              type="radio"
-            />
-            <label htmlFor={_id}>
-              <p className="name">{name}</p>
-              <p className="address">
-                {street}, {city},{state}, {country} {pincode} - {phone}
-              </p>
-            </label>
-          </div>
-        );
-      })}
-      <div className="add-new-address-btn-container">
-        <button
-          className="add-new-address-btn"
-          onClick={() => setIsAddressModalOpen(true)}
-        >
-          Add New Address
-        </button>
-      </div>
+				return (
+					<div
+						key={_id}
+						className='address-card'>
+						<input
+							checked={
+								_id ===
+								userDataState.orderDetails?.orderAddress?._id
+							}
+							onChange={() => {
+								dispatch({
+									type: 'SET_ORDER',
+									payload: { orderAddress: address },
+								});
+							}}
+							name='address'
+							id={_id}
+							type='radio'
+						/>
+						<label htmlFor={_id}>
+							<p className='name'>{name}</p>
+							<p className='address'>
+								{street}, {city},{state}, {country} {pincode} -{' '}
+								{phone}
+							</p>
+						</label>
+					</div>
+				);
+			})}
+			<div className='add-new-address-btn-container'>
+				<button
+					className='add-new-address-btn'
+					onClick={() => setIsAddressModalOpen(true)}>
+					Add New Address
+				</button>
+			</div>
 
-      {isAddressModalOpen && <AddressModal />}
-    </div>
-  );
+			{isAddressModalOpen && <AddressModal />}
+		</div>
+	);
 };
